3179bcfb62d6f02c014db4a701df631f9318fc5f
icellus <2283411628@qq.com>
2017-10-24 17:46:39 +0800

    add barryvdh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+		// Barryvdh
+		if ($this->app->environment() != 'production') {
+			$this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+			$this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+		}
     }
 }
